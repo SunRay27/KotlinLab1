85cc9812a4f582e2966398ae9eaad938c7a9d082
Yaroslav <pirotexnic27@yandex.ru>
2022-03-17 19:41:10 +0300

    -main fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,21 @@ fun main() {
 
     parseResult.forEach { println(it) }
 
-    println()
 
-    println("Address with max index: ${parseResult.maxByOrNull { i -> i.index }}")
-    println("Address with min index: ${parseResult.minByOrNull { i -> i.index }}")
+    println("\nAddresses with max index:")
+    val maxIndexValue = parseResult.maxOf { i -> i.index }
+    parseResult.forEach{if(it.index == maxIndexValue) println(it)}
 
-    println("Address with longest street name: ${parseResult.maxByOrNull { i -> i.streetName.length }}")
-    println("Address with shortest street name: ${parseResult.minByOrNull { i -> i.streetName.length }}")
+    println("\nAddresses with min index:")
+    val minIndexValue = parseResult.minOf { i -> i.index }
+    parseResult.forEach{if(it.index == minIndexValue) println(it)}
+
+    println("\nAddresses with longest street name:")
+    val maxStreetLen = parseResult.maxOf { i -> i.streetName.length }
+    parseResult.forEach{if(it.streetName.length == maxStreetLen) println(it)}
+
+    println("\nAddresses with shortest street name:")
+    val minStreetLen = parseResult.minOf { i -> i.streetName.length }
+    parseResult.forEach{if(it.streetName.length == minStreetLen) println(it)}
+
 }
